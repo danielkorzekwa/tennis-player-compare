137b3511dee9370fa91f3f0db4baaa7502f1a07e
Daniel Korzekwa <daniel.korzekwa@gmail.com>
2011-12-30 22:45:23 +0000

    #1 Calculate probability of winning a tennis match.

--- src/main/scala/dk/tennis/compare/ATPTennisMatchCompare.scala
+++ src/main/scala/dk/tennis/compare/ATPTennisMatchCompare.scala
@@ -1,16 +1,18 @@
 package dk.tennis.compare
 
+import dk.atp.api._
 import dk.atp.api.AtpWorldTourApi._
 import SurfaceEnum._
 import dk.tennisprob.TennisProbCalc.MatchTypeEnum._
+import dk.tennisprob._
 
 /**
  * Calculates probability of winning a tennis match by player A against player B. For instance Roger Federer vs Novak Djokovic
  *
  */
-class ATPTennisMatchCompare extends TennisPlayerCompare{
+class ATPTennisMatchCompare(atpApi: AtpWorldTourApiImpl) extends TennisPlayerCompare {
 
-   /**
+  /**
    * Calculates probability of winning a tennis match by player A against player B.
    *
    * @param fullNamePlayerA e.g. Roger Federer
@@ -20,5 +22,29 @@ class ATPTennisMatchCompare extends TennisPlayerCompare{
    * @param year Probability is calculated for the last day of a year.
    * @return Probability between 0 and 1.
    */
-  def matchProb(fullNamePlayerA: String, fullNamePlayerB: String, surface: SurfaceEnum, matchType: MatchTypeEnum, year:Int): Double = throw new UnsupportedOperationException("Not implemented yet.")
+  def matchProb(fullNamePlayerA: String, fullNamePlayerB: String, surface: SurfaceEnum, matchType: MatchTypeEnum, year: Int): Double = {
+
+    val playerAFacts = atpApi.playerFacts(fullNamePlayerA, surface, year)
+    val playerBFacts = atpApi.playerFacts(fullNamePlayerB, surface, year)
+
+    val firstServeAvpProb = atpApi.firstServeFacts(surface, year).firstServeAvgPct() / 100
+
+    val firstReturnAvgProb = atpApi.pointWonFacts(PointWonFactEnum.FIRST_SERVE_RETURN_POINTS_WON, surface, year).pointWonAvgPct() / 100
+    val secondReturnAvgProb = atpApi.pointWonFacts(PointWonFactEnum.SECOND_SERVE_RETURN_POINTS_WON, surface, year).pointWonAvgPct() / 100
+
+    val winOnReturnAvgProb = TennisProbFormulaCalc.pointAvgProb(firstServeAvpProb, firstReturnAvgProb, secondReturnAvgProb)
+
+    val winOnServeProbA = TennisProbFormulaCalc.pointAvgProb(playerAFacts.firstServePct / 100, playerAFacts.firstServeWonPct / 100, playerAFacts.secondServeWonPct / 100)
+    val winOnReturnProbA = TennisProbFormulaCalc.pointAvgProb(firstServeAvpProb, playerAFacts.firstReturnWonPct / 100, playerAFacts.secondReturnWonPct / 100)
+
+    val winOnServeProbB = TennisProbFormulaCalc.pointAvgProb(playerBFacts.firstServePct / 100, playerBFacts.firstServeWonPct / 100, playerBFacts.secondServeWonPct / 100)
+    val winOnReturnProbB = TennisProbFormulaCalc.pointAvgProb(firstServeAvpProb, playerBFacts.firstReturnWonPct / 100, playerBFacts.secondReturnWonPct / 100)
+
+    //calculate match prob
+    val winOnServeAGivenBProb = TennisProbFormulaCalc.pointProb(winOnServeProbA, winOnReturnProbB, winOnReturnAvgProb)
+    val winOnServeBGivenAProb = TennisProbFormulaCalc.pointProb(winOnServeProbB, winOnReturnProbA, winOnReturnAvgProb)
+
+    val matchProbAGivenB = TennisProbFormulaCalc.matchProb(winOnServeAGivenBProb, 1 - winOnServeBGivenAProb, matchType)
+    matchProbAGivenB
+  }
 }
--- src/test/scala/dk/tennis/compare/ATPTennisMatchCompareTest.scala
+++ src/test/scala/dk/tennis/compare/ATPTennisMatchCompareTest.scala
@@ -5,45 +5,55 @@ import Assert._
 import dk.atp.api.AtpWorldTourApi._
 import SurfaceEnum._
 import dk.tennisprob.TennisProbCalc.MatchTypeEnum._
+import dk.atp.api.AtpWorldTourApiImpl
 
 class ATPTennisMatchCompareTest {
 
-  private val matchCompare = new ATPTennisMatchCompare()
+  private val atpApi = new AtpWorldTourApiImpl()
+  private val matchCompare = new ATPTennisMatchCompare(atpApi)
 
   @Test def matchProb_Roger_Federer_vs_Milos_Raonic {
 
     val playerAFullName = "Roger Federer"
-    val playerBFullName = "Milos_Raonic"
+    val playerBFullName = "Milos Raonic"
 
-    assertEquals(-1, matchCompare.matchProb(playerAFullName, playerBFullName, CLAY, THREE_SET_MATCH, 2011), 0)
-    assertEquals(-1, matchCompare.matchProb(playerAFullName, playerBFullName, CLAY, FIVE_SET_MATCH, 2011), 0)
+    assertEquals(0.768, matchCompare.matchProb(playerAFullName, playerBFullName, CLAY, THREE_SET_MATCH, 2011), 0.001)
+    assertEquals(0.231, matchCompare.matchProb(playerBFullName, playerAFullName, CLAY, THREE_SET_MATCH, 2011), 0.001)
 
-    assertEquals(-1, matchCompare.matchProb(playerAFullName, playerBFullName, GRASS, THREE_SET_MATCH, 2011), 0)
-    assertEquals(-1, matchCompare.matchProb(playerAFullName, playerBFullName, HARD, THREE_SET_MATCH, 2011), 0)
+    assertEquals(0.820, matchCompare.matchProb(playerAFullName, playerBFullName, CLAY, FIVE_SET_MATCH, 2011), 0.001)
 
-    assertEquals(-1, matchCompare.matchProb(playerAFullName, playerBFullName, CLAY, THREE_SET_MATCH, 2010), 0)
+    assertEquals(0.749, matchCompare.matchProb(playerAFullName, playerBFullName, GRASS, THREE_SET_MATCH, 2011), 0.001)
+    assertEquals(0.8366, matchCompare.matchProb(playerAFullName, playerBFullName, HARD, THREE_SET_MATCH, 2011), 0.001)
+
+    //exception should be thrown here - no data for playerB
+    assertEquals(-1, matchCompare.matchProb(playerAFullName, playerBFullName, CLAY, THREE_SET_MATCH, 2010), 0.001)
+
   }
 
   @Test def matchProb_various_players_2011 {
-    assertEquals(-1, matchCompare.matchProb("Novak Djokovic", "Rafael Nadal", CLAY, THREE_SET_MATCH, 2011), 0)
-    assertEquals(-1, matchCompare.matchProb("Novak Djokovic", "Rafael Nadal", GRASS, THREE_SET_MATCH, 2011), 0)
-    assertEquals(-1, matchCompare.matchProb("Novak Djokovic", "Rafael Nadal", HARD, THREE_SET_MATCH, 2011), 0)
+    assertEquals(0.489, matchCompare.matchProb("Novak Djokovic", "Rafael Nadal", CLAY, THREE_SET_MATCH, 2011), 0.001)
+    assertEquals(0.717, matchCompare.matchProb("Novak Djokovic", "Rafael Nadal", GRASS, THREE_SET_MATCH, 2011), 0.001)
+    assertEquals(0.669, matchCompare.matchProb("Novak Djokovic", "Rafael Nadal", HARD, THREE_SET_MATCH, 2011), 0.001)
+    
+     assertEquals(0.087, matchCompare.matchProb("Novak Djokovic", "Rafael Nadal", CLAY, THREE_SET_MATCH, 2010), 0.001)
+    assertEquals(0.358, matchCompare.matchProb("Novak Djokovic", "Rafael Nadal", GRASS, THREE_SET_MATCH, 2010), 0.001)
+    assertEquals(0.4034, matchCompare.matchProb("Novak Djokovic", "Rafael Nadal", HARD, THREE_SET_MATCH, 2010), 0.001)
 
-    assertEquals(-1, matchCompare.matchProb("Rafael Nadal", "Roger Federer", CLAY, THREE_SET_MATCH, 2011), 0)
-    assertEquals(-1, matchCompare.matchProb("Rafael Nadal", "Roger Federer", GRASS, THREE_SET_MATCH, 2011), 0)
-    assertEquals(-1, matchCompare.matchProb("Rafael Nadal", "Roger Federer", HARD, THREE_SET_MATCH, 2011), 0)
+    assertEquals(0.761, matchCompare.matchProb("Rafael Nadal", "Roger Federer", CLAY, THREE_SET_MATCH, 2011), 0.001)
+    assertEquals(0.186, matchCompare.matchProb("Rafael Nadal", "Roger Federer", GRASS, THREE_SET_MATCH, 2011), 0.001)
+    assertEquals(0.304, matchCompare.matchProb("Rafael Nadal", "Roger Federer", HARD, THREE_SET_MATCH, 2011), 0.001)
 
-    assertEquals(-1, matchCompare.matchProb("Novak Djokovic", "Roger Federer", CLAY, THREE_SET_MATCH, 2011), 0)
-    assertEquals(-1, matchCompare.matchProb("Novak Djokovic", "Roger Federer", GRASS, THREE_SET_MATCH, 2011), 0)
-    assertEquals(-1, matchCompare.matchProb("Novak Djokovic", "Roger Federer", HARD, THREE_SET_MATCH, 2011), 0)
+    assertEquals(0.747, matchCompare.matchProb("Novak Djokovic", "Roger Federer", CLAY, THREE_SET_MATCH, 2011), 0.001)
+    assertEquals(0.366, matchCompare.matchProb("Novak Djokovic", "Roger Federer", GRASS, THREE_SET_MATCH, 2011), 0.001)
+    assertEquals(0.464, matchCompare.matchProb("Novak Djokovic", "Roger Federer", HARD, THREE_SET_MATCH, 2011), 0.001)
 
-    assertEquals(-1, matchCompare.matchProb("Novak Djokovic", "Robin Soderling", CLAY, THREE_SET_MATCH, 2011), 0)
-    assertEquals(-1, matchCompare.matchProb("Novak Djokovic", "Robin Soderling", GRASS, THREE_SET_MATCH, 2011), 0)
-    assertEquals(-1, matchCompare.matchProb("Novak Djokovic", "Robin Soderling", HARD, THREE_SET_MATCH, 2011), 0)
+    assertEquals(0.783, matchCompare.matchProb("Novak Djokovic", "Robin Soderling", CLAY, THREE_SET_MATCH, 2011), 0.001)
+    assertEquals(0.980, matchCompare.matchProb("Novak Djokovic", "Robin Soderling", GRASS, THREE_SET_MATCH, 2011), 0.001)
+    assertEquals(0.642, matchCompare.matchProb("Novak Djokovic", "Robin Soderling", HARD, THREE_SET_MATCH, 2011), 0.001)
 
-    assertEquals(-1, matchCompare.matchProb("Roger Federer", "Robin Soderling", CLAY, THREE_SET_MATCH, 2011), 0)
-    assertEquals(-1, matchCompare.matchProb("Roger Federer", "Robin Soderling", GRASS, THREE_SET_MATCH, 2011), 0)
-    assertEquals(-1, matchCompare.matchProb("Roger Federer", "Robin Soderling", HARD, THREE_SET_MATCH, 2011), 0)
+    assertEquals(0.534, matchCompare.matchProb("Roger Federer", "Robin Soderling", CLAY, THREE_SET_MATCH, 2011), 0.001)
+    assertEquals(0.990, matchCompare.matchProb("Roger Federer", "Robin Soderling", GRASS, THREE_SET_MATCH, 2011), 0.001)
+    assertEquals(0.667, matchCompare.matchProb("Roger Federer", "Robin Soderling", HARD, THREE_SET_MATCH, 2011), 0.001)
 
     assertEquals(-1, matchCompare.matchProb("Novak Djokovic", "Michael Russell", CLAY, THREE_SET_MATCH, 2011), 0)
     assertEquals(-1, matchCompare.matchProb("Novak Djokovic", "Michael Russell", GRASS, THREE_SET_MATCH, 2011), 0)
