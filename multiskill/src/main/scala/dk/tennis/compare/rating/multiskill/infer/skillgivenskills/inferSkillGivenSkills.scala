8ad082845d0caf2f433ad9dab1dde8506f6ace8c
Daniel Korzekwa <daniel.korzekwa@gmail.com>
2014-10-15 17:45:23 +0100

    Infering player skill given other players skills.

---
+++ multiskill/src/main/scala/dk/tennis/compare/rating/multiskill/infer/skillgivenskills/inferSkillGivenSkills.scala
@@ -0,0 +1,38 @@
+package dk.tennis.compare.rating.multiskill.infer.skillgivenskills
+
+import dk.bayes.math.gaussian.Gaussian
+import dk.bayes.math.gaussian.MultivariateGaussian
+import dk.tennis.compare.rating.multiskill.model.perfdiff.Player
+import dk.bayes.math.linear.Matrix
+import dk.tennis.compare.rating.multiskill.model.perfdiff.skillsfactor.multigp.cov.CovFunc
+import dk.bayes.math.linear.Matrix
+
+/**
+ * Infer player skill given other players skills
+ */
+object inferSkillGivenSkills {
+
+  def apply(players: Array[Player], x: MultivariateGaussian, z: Player, playerCovFunc: CovFunc, skillMeanFunc: (Player) => Double): Gaussian = {
+
+    /** A and Kz_x parameters for p(z|x)*/
+
+    val KxxInv = x.v.inv
+    val Kzx = playerCovFunc.covarianceMatrix(Array(z), players)
+    val Kxz = playerCovFunc.covarianceMatrix(players, Array(z))
+    val Kzz = playerCovFunc.covarianceMatrix(Array(z))
+
+    val xSkillMean = Matrix(players.map(p => skillMeanFunc(p)))
+    val zSkillMean = Matrix(skillMeanFunc(z))
+
+    val A = Kzx * KxxInv
+
+    val Kz_x = Kzz - Kzx * KxxInv * Kxz
+
+    /** RUN INFERENCE - Compute p(z) = integral of p(x)*p(z|x)dx*/
+    val skillMean = zSkillMean + A * (x.m - xSkillMean)
+    val skillVar = Kz_x + A * x.v * A.t
+
+    Gaussian(skillMean(0), skillVar(0))
+  }
+
+}
---
+++ multiskill/src/test/scala/dk/tennis/compare/rating/multiskill/infer/skillgivenskills/inferSkillGivenSkillsTest.scala
@@ -0,0 +1,48 @@
+package dk.tennis.compare.rating.multiskill.infer.skillgivenskills
+
+import org.junit._
+import Assert._
+import dk.tennis.compare.rating.multiskill.model.perfdiff.Player
+import java.util.Date
+import dk.bayes.math.linear.Matrix
+import dk.bayes.math.gaussian.MultivariateGaussian
+import dk.bayes.math.linear.Matrix
+import dk.tennis.compare.rating.multiskill.model.perfdiff.skillsfactor.multigp.cov.skillovertime.SkillOverTimeCovFunc
+import scala.math._
+
+class inferSkillGivenSkillsTest {
+
+  val day = 1000L * 3600 * 24
+  val skillCovFunc = SkillOverTimeCovFunc(List(log(0.3), log(30), log(1), log(365)))
+
+  val skillMeanFunc = (player: Player) => 0.6
+
+  val players = Array(
+    Player("p1", "p2", onServe = true, timestamp = new Date(day)),
+    Player("p1", "p2", onServe = true, timestamp = new Date(30 * day)),
+    Player("p1", "p3", onServe = true, timestamp = new Date(5 * day)))
+
+  val skillsMean = Matrix(0.2, 0.8, -0.1)
+  val skillsVar = skillCovFunc.covarianceMatrix(players)
+
+  val skillsGaussian = MultivariateGaussian(skillsMean, skillsVar)
+
+  @Test def test {
+
+    val player = Player("p1", "p5", onServe = true, timestamp = new Date(1 * day))
+
+    val playerSkill = inferSkillGivenSkills(players, skillsGaussian, player, skillCovFunc, skillMeanFunc)
+    assertEquals(0.1950, playerSkill.m, 0.0001)
+    assertEquals(1.0900, playerSkill.v, 0.0001)
+  }
+
+  @Test def test_converge_to_mean_value {
+
+    val player = Player("p1", "p5", onServe = true, timestamp = new Date(1200 * day))
+
+    val playerSkill = inferSkillGivenSkills(players, skillsGaussian, player, skillCovFunc, skillMeanFunc)
+
+    assertEquals(0.6174, playerSkill.m, 0.0001)
+    assertEquals(1.0900, playerSkill.v, 0.0001)
+  }
+}
