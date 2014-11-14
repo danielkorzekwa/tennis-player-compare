a4b797ec7dfa831aa3ddedbf0fe4e87b14b76fd6
danielkorzekwa <daniel.korzekwa@gmail.com>
2014-11-14 09:14:54 +0000

    Testing multi skill model.

--- model-tester/src/test/scala/dk/tennis/compare/MatchPredictionApp.scala
+++ model-tester/src/test/scala/dk/tennis/compare/MatchPredictionApp.scala
@@ -14,40 +14,42 @@ import java.util.Date
 object MatchPredictionApp extends App with Logging {
 
   val df = new SimpleDateFormat("dd/MM/yyyy")
-  val player1 = "Tomas Berdych"
-  val player2 = "Marin Cilic"
-
   val matchesFile = "./src/test/resources/atp_historical_data/match_data_2006_2014_121114.csv"
   val matchResults = MatchesLoader.loadMatches(matchesFile, 2012, 2014)
   logger.info("All matches:" + matchResults.size)
   logger.info("All players:" + matchResults.flatMap(m => List(m.player1, m.player2)).distinct.size)
 
+  val player1 = "Stan Wawrinka"
+  val player2 = "Marin Cilic"
   val p1Matches = matchResults.filter(m => m.containsPlayer(player1))
   val p2Matches = matchResults.filter(m => m.containsPlayer(player2))
 
-  //val matchModel = InferMatchProbGivenPastMatchResults(matchResults.toIndexedSeq)
-  val matchModel = InferMatchProbGivenMatchResults(matchResults.toIndexedSeq)
-  logger.info("Predicting...")
-
-  val startTime = df.parse("01/01/2006")
-  val endTime = df.parse("12/11/2016")
+  val startTime = df.parse("01/01/2014")
+  val endTime = df.parse("14/11/2014")
   val MONTH: Long = 1000L * 3600 * 24 * 30
 
-  val predictions = (startTime.getTime to endTime.getTime by MONTH).map { t =>
+  makePredictions()
 
-    val time = new Date(t)
-    val result = MatchResult(time, "tournament name", Surface.HARD, player1, player2, time, player1Won = true, numOfSets = 2, PlayerStats(0, 0, 0), PlayerStats(0, 0, 0))
-    matchModel.predict(result)
+  private def makePredictions() {
+
+    logger.info("Predicting...")
+    val matchModel = InferMatchProbGivenMatchResults(matchResults.toIndexedSeq)
+
+    val predictions = (startTime.getTime to endTime.getTime by MONTH).map { t =>
+
+      val time = new Date(t)
+      val result = MatchResult(time, "tournament name", Surface.HARD, player1, player2, time, player1Won = true, numOfSets = 2, PlayerStats(0, 0, 0), PlayerStats(0, 0, 0))
+      matchModel.predict(result)
+    }
+
+    val lastPrediction = matchModel.predict(MatchResult(endTime, "tournament name", Surface.HARD, player1, player2, endTime, player1Won = true, numOfSets = 2, PlayerStats(0, 0, 0), PlayerStats(0, 0, 0)))
+
+    logger.info("Done...")
+
+    (predictions :+ lastPrediction).foreach(p => println(p.matchTime + ":" + p.matchProb(player1)))
+
+    println("\np1 wins prob=" + lastPrediction.matchProb(player1) + ":" + 1d / lastPrediction.matchProb(player1) + ":" + 1d / (1 - lastPrediction.matchProb(player1)))
+    println("p1 matches=" + p1Matches.size)
+    println("p2 matches=" + p2Matches.size)
   }
-
-  val lastPrediction = predictions.last
-
-  logger.info("Done...")
-
-  predictions.foreach(p => println(p.matchTime + ":" + p.matchProb(player1)))
-
-  println("p1 wins prob=" + lastPrediction.matchProb(player1) + ":" + 1d / lastPrediction.matchProb(player1) + ":" + 1d / (1 - lastPrediction.matchProb(player1)))
-  println("p1 matches=" + p1Matches.size)
-  println("p2 matches=" + p2Matches.size)
-  InferSkillGivenPlayer
 }
